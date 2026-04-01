0d09532748dff453a4e5ab61f57e637a09cd91c9
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-01 15:40:08 -0300

    feat: criando entidade para Cliente

--- Codigo/lab2/lab2/src/main/java/com/aluguel/model/ClienteEntity.java
+++ Codigo/lab2/lab2/src/main/java/com/aluguel/model/ClienteEntity.java
@@ -1,10 +1,52 @@
 package com.aluguel.model;
 
+import io.micronaut.data.annotation.Id;
+import jakarta.persistence.Entity;
+
+@Entity
 public class ClienteEntity {
+    @Id
+    private String rg;
+    private String cpf;
+    private String profissao;
+    private String nome;
+
+    public ClienteEntity(String rg, String cpf, String profissao, String nome) {
+        this.rg = rg;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.nome = nome;
+    }
 
     public Object getId() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+        return rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private void solicitarAluguel() {
+        // TODO lógica para solicitar aluguel
     }
     
+    private void modificarPedido() {
+        // TODO lógica para modificar pedido
+    }
+
+    private void cancelarPedido() {
+        // TODO lógica para cancelar pedido
+    }
+
+    private void assinarContrato() {
+        // TODO lógica para assinar contrato
+    }
 }
