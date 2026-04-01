a6d0b769e06c6d1dc0803a828fc64df6a0b849ee
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-01 15:22:42 -0300

    criando Controller para Cliente

--- Codigo/lab2/lab2/src/main/java/com/aluguel/model/ClienteEntity.java
+++ Codigo/lab2/lab2/src/main/java/com/aluguel/model/ClienteEntity.java
@@ -1,5 +1,10 @@
 package com.aluguel.model;
 
 public class ClienteEntity {
+
+    public Object getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
     
 }
