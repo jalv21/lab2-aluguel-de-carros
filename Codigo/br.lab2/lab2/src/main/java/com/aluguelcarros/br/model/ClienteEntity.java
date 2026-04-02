14b56c61e8027d2dfab5c2237e5de8d2e400213f
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-02 15:14:46 -0300

    feat: criando entidades para Usuario e Cliente

--- Codigo/br.lab2/lab2/src/main/java/com/aluguelcarros/br/model/ClienteEntity.java
+++ Codigo/br.lab2/lab2/src/main/java/com/aluguelcarros/br/model/ClienteEntity.java
@@ -1,5 +1,49 @@
 package com.aluguelcarros.br.model;
 
-public class ClienteEntity {
-    
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ClienteEntity extends UsuarioEntity {
+    @Id
+    private String rg;
+    private String cpf;
+    private String profissao;
+
+    public String getRg() {
+        return rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public ClienteEntity(long id, String login, String senha, String nome, String endereco) {
+        super(id, login, senha, nome, endereco);
+    }
+
+    @Override
+    public boolean autenticar(String login, String senha) {
+        return this.login.equals(login) && this.senha.equals(senha);
+    }
+
+    public void solicitarAluguel() {
+        // TODO
+    }
+
+    public void modificarPedido() {
+        // TODO
+    }
+
+    public void cancelarPedido() {
+        // TODO
+    }
+
+    public void assinarContrato() {
+        // TODO
+    }
 }
--- Codigo/br.lab2/lab2/src/main/java/com/aluguelcarros/br/model/UsuarioEntity.java
+++ Codigo/br.lab2/lab2/src/main/java/com/aluguelcarros/br/model/UsuarioEntity.java
@@ -1,5 +1,24 @@
 package com.aluguelcarros.br.model;
 
-public class UsuarioEntity {
-    
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public abstract class UsuarioEntity {
+    @Id
+    protected long id;
+    protected String login;
+    protected String senha;
+    protected String nome;
+    protected String endereco;
+
+    public UsuarioEntity(long id, String login, String senha, String nome, String endereco) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public abstract boolean autenticar(String login, String senha);
 }
